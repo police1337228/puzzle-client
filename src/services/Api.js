064a9ba3e6e64bb72b1fd76f3f2dfbc83dbf5f73
Ilya Mityaev <ilyamityaev@yahoo.com>
2023-06-21 16:08:01 +0300

    added proxy

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import store from "@/store/index";
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8081/`,
+    baseURL: process.env.VUE_APP_SERVER || `http://localhost:8081/`,
     headers: {
       Authorization: `Bearer ${store.state.auth.token}`,
     },
