152c4b8ac8ad8d8b0d154d88aaf134ecf31841da
Ilya Mityaev <ilyamityaev@yahoo.com>
2023-06-24 22:43:06 +0300

    some optimizations

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
 const { defineConfig } = require("@vue/cli-service");
+const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
+const webpack = require("webpack");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new VuetifyLoaderPlugin(),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^\.\/locale$/,
+        contextRegExp: /moment$/,
+      }),
+    ],
+  },
   pwa: {
     name: "ПАЗЛ - Изучение русского жестового языка онлайн",
   },
